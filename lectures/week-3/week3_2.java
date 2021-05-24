79ab017ef3189b19b6e4e93430cdd421fb3980af
ntrov <wajahatraza11@gmail.com>
2021-05-24 05:48:58 -0700

    new lectures

---
+++ lectures/week-3/week3_2.java
@@ -0,0 +1,15 @@
+
+//shahzad.khan@nu.edu.pk
+
+interface singleMethod{
+    public void sum(int a,int b);
+}
+
+public class week3_2 {
+    public static void main(String[] args) {
+
+        singleMethod obj=(int x,int y) -> System.out.println("sum= "+(x+y));
+            
+        obj.sum(2,2);
+    }
+}
